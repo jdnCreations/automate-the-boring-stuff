d1dd0ac8b38ca19e163d03fbee13f5e1385537a0
Jordan Needham <jdncreations@hotmail.com>
2023-05-24 15:24:34 +0800

    Added delete & deleteall keywords

--- mcb.py
+++ mcb.py
@@ -4,6 +4,10 @@ mcbShelf = shelve.open('mcb')
 
 if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
     mcbShelf[sys.argv[2]] = pyperclip.paste()
+elif sys.argv[1].lower() == 'deleteall':
+    mcbShelf.clear()
+elif len(sys.argv) == 3 and sys.argv[1].lower() == 'delete':
+    del mcbShelf[sys.argv[2]]
 elif len(sys.argv) == 2:
     # List keywords and load content
     if sys.argv[1].lower() == 'list':
